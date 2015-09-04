0ff995bd0ff64d587c452ff863d9900d5f316230
alihitawala <ali.strider@gmail.com>
2015-09-03 22:51:34 -0500

    Heap - Tree map

--- src/Coding/IBit/Heap/MergeKList.java
+++ src/Coding/IBit/Heap/MergeKList.java
@@ -1,7 +1,57 @@
 package Coding.IBit.Heap;
 
+import Coding.IBit.LinkedList.ListNode;
+
+import java.util.*;
+
 /**
  * Created by aliHitawala on 9/1/15.
  */
 public class MergeKList {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        ArrayList<ListNode> lists = new ArrayList<>();
+        for(int j=0;j<t;j++){
+            int n = in.nextInt();
+            ArrayList<Integer> a = new ArrayList<Integer>();
+            for (int i = 0; i < n; i++) {
+                int x =in.nextInt();
+                a.add(x);
+            }
+            ListNode head1 = ListNode.getList(a);
+            lists.add(head1);
+        }
+        ListNode.print(mergeKLists(lists));
+    }
+
+    public static ListNode mergeKLists(ArrayList<ListNode> a) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int i = 0; i <a.size(); i++) {
+            ListNode list = a.get(i);
+            while (list != null) {
+                if (map.containsKey(list.val))
+                {
+                    map.put(list.val, map.get(list.val) + 1);
+                }
+                else
+                    map.put(list.val, 1);
+                list = list.next;
+            }
+        }
+        if (map.isEmpty())
+            return null;
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int val = entry.getKey();
+            int freq = entry.getValue();
+            for (int i = 0; i < freq; i++) {
+                ListNode n = new ListNode(val);
+                temp.next = n;
+                temp = temp.next;
+            }
+        }
+        return head.next;
+    }
 }
--- src/Coding/IBit/LinkedList/ListNode.java
+++ src/Coding/IBit/LinkedList/ListNode.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class ListNode {
     public int val;
     public ListNode next;
-    ListNode(int x) { val = x; next = null; }
+    public ListNode(int x) { val = x; next = null; }
 
     public static ListNode getList(List<Integer> list) {
         ListNode head = new ListNode(list.get(0));
